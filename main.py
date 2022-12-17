52c1e4e89996d66a58defd0495c1faf4ff4b2491
Rmcgonagle22 <113391575+Rmcgonagle22@users.noreply.github.com>
2022-12-17 18:45:30 -0500

    RmcgonagleProject7

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-def reverseString(string):
-	if len(string) <= 1:
-		return string
-
-	return (String[-1] + reverseString(string)[:-1])
-test_string = "This is a test string to test reversal!"
-print(test_string)
-print(reverseString(test_string))
-
-def fib(x):
-	if(x== 1):
-		return 1
-	elif x == 0:
-		return 0
-	else:
-		returnfib(x-1) + fib(x - 2)
-def fibIter(x):
-	1 = 0
-	a = 0
-	b = 1
-	while i < x:
-		c = a + b
-		a = b
-		b = c
-		1 += 1
-	print(b)
-fib(50)
+def compound_interest(initial_balance, interest_rate,years_past):
+    if years_past == 0:
+        return init_bal
+    if init_bal == 0:
+        return 0
