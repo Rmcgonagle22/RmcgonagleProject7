f979f3fce078499a4420e5511032f1b81d4c19f6
Rmcgonagle22 <113391575+Rmcgonagle22@users.noreply.github.com>
2022-12-17 22:40:01 -0500

    RmcgonagleProject7

--- main.py
+++ main.py
@@ -37,3 +37,21 @@ def main():
 #    for stu in student_data:
 #        print(stu)
 
+def draw_triangle(length):
+    pen.setheading(180)      # set the direction of the pen to left
+    for i in range(3):       # draw 3 sides
+        pen.rt(120)          # rotate the pen 120 degrees clockwise
+        pen.fd(length)
+
+import arcade #type: ignore
+def ClassArcade():
+    win = Window.Window(500, 500)
+    win.load()
+    arcade.run()
+
+def draw_triangle(length):
+    pen.setheading(180)      # set the direction of the pen to left
+    for i in range(3):       # draw 3 sides
+        pen.rt(120)          # rotate the pen 120 degrees clockwise
+        pen.fd(length)       # draw side
+                             # pen will end facing left (180)
