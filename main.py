0889d848148ba157c9c4630a619cebf62f6fabcb
Rmcgonagle22 <113391575+Rmcgonagle22@users.noreply.github.com>
2022-12-17 21:29:21 -0500

    RmcgonagleProject7

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ def compound_interest(initial_balance, interest_rate,years_past):
         return init_bal
     if init_bal == 0:
         return 0
+
+def reverseString(string):
+    if len(string) <= 1:
+        return string
+
+    return string[-1] + reverseString(string[:-1])
+
+print(Palidrome)
+print(reverseString(Palidrome))
+
+def fib(x):
+    if(x == 1):
+        return 1
+    elif x == 0:
+        return 0
+    else:
+        return fib(x-1) + fib(x - 2)
+
+def fibIter(x):
+    i = 0
+    a = 0
+    b = 1
+    while i < x:
+        c = a + b
+        a = b
+        b = c
+        i += 1
+    print(b)
